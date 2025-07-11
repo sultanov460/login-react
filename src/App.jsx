225b5437ada547c56906b293fdf40d8a454340f2
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2025-07-11 21:22:21 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import login from "./components/login";
+export default function App() {
+  return (
+    <div>
+      <login />
+    </div>
+  );
+}
---
+++ src/components/login.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+export default function login() {
+  return (
+    <form>
+      <input type="text" placeholder="Enter username" />
+      <button>Login</button>
+      <h4></h4>
+    </form>
+  );
+}
